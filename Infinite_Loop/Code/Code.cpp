2319031a23c375eaebc4067cbf4718b15dec16e8
Eli-Berg-JSCC <122478290+Eli-Berg-JSCC@users.noreply.github.com>
2023-02-16 12:22:32 -0600

    Update Code.cpp

--- Infinite_Loop/Code/Code.cpp
+++ Infinite_Loop/Code/Code.cpp
@@ -1,7 +1,10 @@
 // Lab 1.3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
+/*The reason for the infinite loop was because if the input was a string
+instead of an integer the switch would not allow it to run*/
 
 #include <iostream>
+#include <string> //allows for getline() function
 using namespace std;
 
 int main()
@@ -9,7 +12,7 @@ int main()
 	int choice = 0;
 
 	string name = "";
-	string age = "";
+	int age = 0; //Allow for easier adding at end of program
 	string occup = "";
 
 	while (choice != -1)
@@ -18,24 +21,40 @@ int main()
 		cout << "1: Enter Name\n";
 		cout << "2: Enter Age\n";
 		cout << "3: Enter Occupation\n";
-		cin >> choice;
+		//cin >> choice;
 
-		/*if (choice == -1)
-			break;*/
+		while (!(cin >> choice)) //This checks the input for something that is not an integer  
+		{
+			cin.clear();  		//And if its something else choice will be set at 0 and then go to default
+			cin.ignore(100, '\n');
+			int choice = 0;
+			break;
+		}
 
+		if (choice == -1) { 
+			break;
+		}
 		switch (choice)
 		{
 		case 1:
 			cout << "What is your Name: ";
-			cin >> name;
+			getline(cin >> ws, name); //getline instead of one word
 			break;
 		case 2:
 			cout << "What is your Age? ";
 			cin >> age;
+			while (!cin) //Testin g if input is integer
+			{
+				cin.clear(); // Clears the input
+				cin.ignore(100, '\n'); //only allows strings up to 100 characters
+				cout << "Invalid response, please enter a digit.\n" << "What is your Age: ";
+				cin >> age;
+			}
 			break;
+
 		case 3:
 			cout << "What is your Occupation? ";
-			cin >> occup;
+			getline(cin >> ws, occup); //getline instead of one word
 			break;
 		default:
 			// Assume Invalid Menu Choice
@@ -44,10 +63,10 @@ int main()
 		}
 	}
 
-	age += 1;
+	age += 1; 
 
 	cout << "Thank you for using our application, " << name << " and hope your career in " << occup << " is successful.";
-	cout << "Hope your " << age << " birthday will enjoyable and exciting";
+	cout << "Hope your " << age << " birthday will be enjoyable and exciting";
 
 	return 0;
 }
